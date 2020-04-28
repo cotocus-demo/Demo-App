9709eea08194b8d200dd2b857a1533ba03cc33d3
Deepak Kumar <deepakkumar.ac.in@gmail.com>
2020-04-28 12:05:44 +0530

    Developed Laravel project from scratch 1.Upgraded Botstrap 3 to Bootstrap 4 2.Customise Default Login and Register view. 3.Changed Default laravel auth login view by overriding showLoginForm(){} function of AuthenticatesUsers trait in login Controller.

--- README.md
+++
@@ -1,2 +0,0 @@
-# Demo-App
-Build Laravel Application from scratch with Login and Register functionality. Used Bootstrap 4 from web pages.
---
+++ resources/views/auth/login-backup.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-body">
+                    <h2>Login</h2>
+                    <form method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email">Email:</label>
+                            <input id="email" type="email" class="form-control" placeholder="Enter email" name="email" value="{{ old('email') }}" required autofocus>
+
+                            @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+
+                        </div>
+
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+
+                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <input id="pwd" type="password" class="form-control" name="password" placeholder="Enter password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+
+                        </div>
+
+                        <div class="form-group form-check">
+                            <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
+                            </label>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
